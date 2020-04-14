e29f1602e8264edc36e644d79f432414e6bf57d7
VictorLCosta <victorlc2019@outlook.com>
2020-04-14 14:45:17 -0300

    Class product and implementation in main program completed

---
+++ Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace LambdaExpressions.Entities
+{
+    class Product
+    {
+        public string Name { get; private set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name.ToUpper()}, R${Price.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
+using LambdaExpressions.Entities;
+using System.Globalization;
+using System.Linq;
 
 namespace LambdaExpressions
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Product> products = new List<Product>();
+            Console.WriteLine("Products list");
+
+            while (true) 
+            {
+                Console.Write("Product name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                products.Add(new Product(name, price));
+
+                Console.WriteLine();
+                Console.Write("Insert other product[Y/N]: ");
+                char op = char.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                if (op == 'N' || op == 'n') 
+                {
+                    break;
+                }
+            }
+
+            products.ForEach(p => { p.Price += p.Price * 0.1; });
+            products.Select(p => p in products); 
+
+            foreach (Product prod in products) 
+            {
+                Console.WriteLine(prod);
+            }
         }
     }
 }
